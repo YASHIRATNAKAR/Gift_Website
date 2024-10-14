151a5cc57d59c7dbdbe7cca86e171c539f196499
YASHIRATNAKAR <108261265+YASHIRATNAKAR@users.noreply.github.com>
2024-10-14 22:51:18 +0530

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,7 +6,7 @@ import { useCart } from "../context/cart";
 import axios from "axios";
 import toast from "react-hot-toast";
 import Layout from "./../components/Layout/Layout";
-// import { AiOutlineReload } from "react-icons/ai";
+import { AiOutlineReload } from "react-icons/ai";
 import "../styles/Homepage.css";
 
 const HomePage = () => {
