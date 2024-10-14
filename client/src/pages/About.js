cc4b869062a851328e997014191b09004cd2c30c
YASHIRATNAKAR <108261265+YASHIRATNAKAR@users.noreply.github.com>
2024-10-14 23:32:30 +0530

    Update About.js

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,22 +3,23 @@ import Layout from "./../components/Layout/Layout";
 
 const About = () => {
   return (
-    <Layout title={"About us - Ecommer app"}>
+    <Layout title={"About us - Upahar app"}>
       <div className="row contactus ">
         <div className="col-md-6 ">
           <img
-            src="/images/about.jpeg"
+            src="/images/image1.jpg"
             alt="contactus"
-            style={{ width: "100%" }}
+            style={{ width: "80%", height: "60vh"}}
           />
         </div>
         <div className="col-md-4">
           <p className="text-justify mt-2">
-            Welcome to Upahar, your go-to online store for finding thoughtful gifts for every occasion. 
+           Welcome to Upahar, your go-to online store for finding thoughtful gifts for every occasion. 
            From birthdays to anniversaries, and everything in between, we offer a wide range of curated gifts for men, women, children, and even flowers. 
            With easy navigation and filters, you can quickly find the perfect gift. 
            Our platform ensures a seamless shopping experience with secure login and simple checkout. 
-           At Upahar, we help you make every moment memorable!
+           At Upahar, we help you make every moment memorable.
+
           </p>
         </div>
       </div>
